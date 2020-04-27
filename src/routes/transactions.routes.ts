dba15b0ae842980070d4b57a05d906e38f1463bd
AnaCarol21 <carol.agl@live.com>
2020-04-27 09:29:21 -0300

    host for localhost

--- src/routes/transactions.routes.ts
+++ src/routes/transactions.routes.ts
@@ -44,14 +44,16 @@ transactionsRouter.delete('/:id', async (request, response) => {
   return response.json().status(204);
 });
 
-transactionsRouter.post('/import',
-upload.single('file'),
-async (request, response) => {
-  const importTransactions = new ImportTransactionsService();
+transactionsRouter.post(
+  '/import',
+  upload.single('file'),
+  async (request, response) => {
+    const importTransactions = new ImportTransactionsService();
 
-  const transactions = await importTransactions.execute(request.file.path);
+    const transactions = await importTransactions.execute(request.file.path);
 
-  return response.json(transactions);
-});
+    return response.json(transactions);
+  },
+);
 
 export default transactionsRouter;
